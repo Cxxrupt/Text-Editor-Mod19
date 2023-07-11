45fc9f5562f1255a1a8f88dd4115f8def364c2b7
Cxxrupt <123yaxk456@gmail.com>
2023-07-10 19:02:02 -0700

    client

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,8 +4,27 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+    });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        // The deferred prompt isn't available.
+        return;
+    }
+
+    // Show the install prompt.
+    promptEvent.prompt();
+
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 

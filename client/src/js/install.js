082e0731a099ffcaeeb9fe4998d15a2d901bd347
Cxxrupt <123yaxk456@gmail.com>
2023-07-10 19:15:24 -0700

    Client, sw.js, webpack.config

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,23 +1,25 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
 
+    // Store the triggered events
     window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
-    });
+  });
 
-    butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-        // The deferred prompt isn't available.
-        return;
-    }
+butInstall.addEventListener('click', async () => {
+  
+  const promptEvent = window.deferredPrompt;
 
-    // Show the install prompt.
-    promptEvent.prompt();
+  if (!promptEvent) {
+    return;
+  }
 
+  // Show prompt
+  promptEvent.prompt();
+  
   // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
   
